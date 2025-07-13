05044cedf52a47b15f98f9abdb6e55b62aedd3dd
SenatorTrent <strentmccauley@gmail.com>
2025-07-13 02:52:24 -0700

    Add UI w/js

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -5,13 +5,17 @@ console.log("Howdy fellers");
 
 let humanScore = 0;
 let computerScore = 0;
+let scoreChange = new CustomEvent("scoreChange");
+let noWinner = true;
 
+/*
 counter = 5;
 while (counter > 0){
     playRound(getHumanChoice(), getComputerChoice());
     console.log(`Scores: player has ${humanScore} and bot has ${computerScore}`);
     counter -=1;
 }
+*/
 
 function getComputerChoice(){
     let num = Math.floor(Math.random()*3);
@@ -39,17 +43,44 @@ function getHumanChoice(){
 function loss(){
     computerScore += 1;
     console.log("'puter wins!");
+    gameLog.dispatchEvent(scoreChange);
 }
 function win(){
     humanScore +=1;
     console.log("player wins!");
+    gameLog.dispatchEvent(scoreChange);
 }
 function draw(){
     console.log("It's a draw! No points");
+    gameLog.dispatchEvent(scoreChange);
+}
+function updateScores(){
+    var scores = `player:${humanScore} Vs. robot:${computerScore}`;
+    const li = document.createElement("li");
+    li.textContent = scores;
+    console.log(li);
+    if(humanScore == 5){
+        li.textContent = "Player won!"
+        li.classList.add("winning-text");
+        noWinner = false;
+    }
+    else if(computerScore==5){
+        li.textContent = "robot won!"
+        li.classList.add("winning-text");
+        noWinner = false;
+    }
+    gameLog.prepend(li);
 }
 
 function playRound(humanChoice, computerChoice){
-    console.log(`player:${humanChoice} Vs. bot:${computerChoice}`);
+    if(!noWinner){
+        console.log("Winner already, exit early");
+        return;
+    }
+    gameStateString = `player:${humanChoice} Vs. bot:${computerChoice}`;
+    console.log(gameStateString);
+    gameState.textContent = gameStateString;
+
     if(humanChoice === computerChoice){
         draw();
         return;
@@ -88,3 +119,17 @@ function playRound(humanChoice, computerChoice){
     }
 
 }
+
+const rockBtn = document.querySelector("button.rock");
+rockBtn.addEventListener("click", () => playRound("ROCK", getComputerChoice()));
+
+const paperBtn = document.querySelector("button.paper");
+paperBtn.addEventListener("click", () => playRound("PAPER", getComputerChoice()));
+
+const scissorsBtn = document.querySelector("button.scissors");
+scissorsBtn.addEventListener("click", () => playRound("SCISSORS", getComputerChoice()));
+
+const gameState = document.querySelector("div.gameState");
+
+const gameLog = document.querySelector("ul.gameLog");
+gameLog.addEventListener("scoreChange", () => this.textContent = updateScores());
